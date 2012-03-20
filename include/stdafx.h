dffa471587c9c1c26e63630033411a42b63375dd
abmantis <amfcalt@gmail.com>
2012-03-20 00:50:34 +0000

    added NOMINMAX define in stdafx

--- include/stdafx.h
+++ include/stdafx.h
@@ -7,12 +7,13 @@
 
 // Windows includes
 #ifdef SKETCHTESTER_OS_WIN32
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+#define NOMINMAX						// Stop windows.h from messing up std::min
 // Including SDKDDKVer.h defines the highest available Windows platform.
 // If you wish to build your application for a previous Windows platform, include WinSDKVer.h and
 // set the _WIN32_WINNT macro to the platform you wish to support before including SDKDDKVer.h.
 #include <SDKDDKVer.h>
-
-#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 
 #endif //SKETCHTESTER_OS_WIN32
 
