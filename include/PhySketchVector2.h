7e39226a10c2e95e7bc1d60d77522b56737964fe
abmantis <amfcalt@gmail.com>
2012-06-01 19:56:19 +0100

    Added dotProcut(), normalised() and angleBetween() to Vector2; Added Vector2::UNIT_... and such

--- include/PhySketchVector2.h
+++ include/PhySketchVector2.h
@@ -3,11 +3,20 @@
 #define Vector2_h__
 
 #include "PhySketchDefinitions.h"
+#include "PhySketchUtils.h"
 
 namespace PhySketch
 {
 	class Vector2
 	{
+	public:
+		static const Vector2 ZERO;
+		static const Vector2 UNIT_X;
+		static const Vector2 UNIT_Y;
+		static const Vector2 NEGATIVE_UNIT_X;
+		static const Vector2 NEGATIVE_UNIT_Y;
+		static const Vector2 UNIT_SCALE;
+
 	public:
 		Vector2(void) : x(0), y(0)
 		{
@@ -106,6 +115,15 @@ namespace PhySketch
 			return Vector2(x - scalar, y - scalar);
 		}
 
+		virtual double dotProduct(Vector2 vec) const
+		{
+			return x * vec.x + y * vec.y;
+		}
+
+		virtual Vector2 normalised() const
+		{
+			return (*this) / length();
+		}
 
 		// Squared length of this vector
 		virtual double sqrdLength() const
@@ -141,6 +159,25 @@ namespace PhySketch
 		static double distance(const Vector2& p1, const Vector2& p2)
 		{
 			return (p2 - p1).length();
+		}
+
+		/// <summary> Compute the angle between two vectors. </summary>
+		/// <param name="v1"> The first Vector2; </param>
+		/// <param name="v2"> The second  Vector2 </param>
+		/// <returns> The angle </returns>
+		static double angleBetween(const Vector2& v1, const Vector2& v2)
+		{
+			double len = v1.length() * v2.length();
+			
+			// prevent divide by zero 
+			if(len < 1e-7f)
+			{
+				len = 1e-7f;
+			}
+
+			double val = v1.dotProduct(v2) / len;
+
+			return radiansToDegrees(acos(val));
 		}
 
 		friend std::ostream& operator<<(std::ostream& out, const Vector2& vector)
