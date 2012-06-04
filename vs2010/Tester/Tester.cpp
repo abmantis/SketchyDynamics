2f973f4cc4f1747642f06c479ec09241f64ffb96
abmantis <amfcalt@gmail.com>
2012-06-05 00:53:15 +0100

    Updated Tester project to accommodate latest changes (Box2D related)

--- vs2010/Tester/Tester.cpp
+++ vs2010/Tester/Tester.cpp
@@ -14,7 +14,7 @@ TestInputListener *_inputListener;
 
 int _tmain(int argc, _TCHAR* argv[])
 {	
-	_core.initialise("Log.txt", true);
+	_core.initialise("Log.txt", true, PhySketch::Vector2(0, -10));
 	_window = _core.createWindow("test", 800, 600, false);
 		
 	_inputListener = new TestInputListener();
