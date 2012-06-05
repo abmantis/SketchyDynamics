89491d044880f5a3e499be10f02eda10029d403f
abmantis <amfcalt@gmail.com>
2012-06-06 00:00:16 +0100

    Added physics body to BodyPolygon class (and basic support functions)

--- include/PhySketchBodyPolygon.h
+++ include/PhySketchBodyPolygon.h
@@ -2,6 +2,7 @@
 #define PhySketchBodyPolygon_h__
 
 #include "PhySketchPolygon.h"
+#include "Box2D/Dynamics/b2Body.h"
 
 namespace PhySketch
 {
@@ -11,12 +12,22 @@ namespace PhySketch
 		BodyPolygon(DrawingMode dm = DM_LINES);
 		virtual ~BodyPolygon();
 		
+
 		/// <summary> Updates the Polygon. </summary>
 		/// <remarks> This is normally not called by the "user". It is called automatically by PhySketch. </remarks>
 		virtual void update();
 		
-	protected:		
-		
+		/// <summary> Gets the physics body. </summary>
+		/// <returns> The physics body. </returns>
+		const b2Body* getPhysicsBody(void) const;
+
+		/// <summary> Sets the physics body. </summary>
+		/// <param name="body"> The body. </param>
+		void setPhysicsBody(b2Body* body);
+
+
+	protected:
+		b2Body* _physicsBody;
 	};
 }
 #endif // PhySketchBodyPolygon_h__
