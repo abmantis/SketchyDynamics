8388e58d16644ddcf93c57c8e469207278fed5a3
abmantis <amfcalt@gmail.com>
2012-06-06 00:44:38 +0100

    Changed resolution in Tester project

--- vs2010/Tester/Tester.cpp
+++ vs2010/Tester/Tester.cpp
@@ -15,7 +15,7 @@ TestInputListener *_inputListener;
 int _tmain(int argc, _TCHAR* argv[])
 {	
 	_core.initialise("Log.txt", true, PhySketch::Vector2(0, -10));
-	_window = _core.createWindow("test", 800, 600, false);
+	_window = _core.createWindow("test", 1280, 720, false);
 		
 	_inputListener = new TestInputListener();
 	_window->addInputListener(_inputListener);
