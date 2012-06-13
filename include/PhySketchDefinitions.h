4b0e60f1f82470c5fac9bec096cf5bc77b6cdf5c
abmantis <amfcalt@gmail.com>
2012-06-13 16:57:43 +0100

    Fix PhySketch MACROS in wrong location.

--- include/PhySketchDefinitions.h
+++ include/PhySketchDefinitions.h
@@ -1,3 +1,24 @@
+// Define current OS
+#if defined( __WIN32__ ) || defined( _WIN32 )
+#   define PHYSKETCH_OS_WIN32
+#else
+#   define PHYSKETCH_OS_LINUX
+#endif
+
+/* Find the arch type */
+#if defined(__x86_64__) || defined(_M_X64) || defined(__powerpc64__) || defined(__alpha__) || defined(__ia64__) || defined(__s390__) || defined(__s390x__)
+#   define PHYSKETCH_ARCH_64
+#else
+#   define PHYSKETCH_ARCH_32
+#endif
+
+// Sombunall versions of math.h already define M_PI
+#ifndef M_PI
+// You can extend this approximation as far as you need to;
+// this version was copied from the MINGW GCC headers
+#define M_PI 3.14159265358979323846  
+#endif
+
 //////////////////////////////////////////////////////////////////////////
 // include files
 //
@@ -32,27 +53,6 @@
 
 namespace PhySketch
 {
-	// Define current OS
-	#if defined( __WIN32__ ) || defined( _WIN32 )
-	#   define PHYSKETCH_OS_WIN32
-	#else
-	#   define PHYSKETCH_OS_LINUX
-	#endif
-
-		/* Find the arch type */
-	#if defined(__x86_64__) || defined(_M_X64) || defined(__powerpc64__) || defined(__alpha__) || defined(__ia64__) || defined(__s390__) || defined(__s390x__)
-	#   define PHYSKETCH_ARCH_64
-	#else
-	#   define PHYSKETCH_ARCH_32
-	#endif
-
-	// Sombunall versions of math.h already define M_PI
-	#ifndef M_PI
-		// You can extend this approximation as far as you need to;
-		// this version was copied from the MINGW GCC headers
-		#define M_PI 3.14159265358979323846  
-	#endif
-
 	//////////////////////////////////////////////////////////////////////////
 	/// Custom ASSERT macro
 	void __physketch_assert__(const char * file, unsigned line, const char * expr, bool dump);
