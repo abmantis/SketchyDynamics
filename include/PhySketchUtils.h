c8747a3fe09ce5cbd358fbcebfec93d988214189
abmantis <amfcalt@gmail.com>
2012-06-17 23:50:18 +0100

    Add readFileContents() to Utils

--- include/PhySketchUtils.h
+++ include/PhySketchUtils.h
@@ -37,5 +37,31 @@ namespace PhySketch
 	{
 		return (long)floor(d + 0.5);
 	}
+
+	/// <summary> Reads a file contents. </summary>
+	/// <param name="filename"> Path to the file. </param>
+	/// <param name="length"> [out] The length of the file. </param>
+	/// <returns> null if it fails, else the file contents. </returns>
+	static void* readFileContents(const char *filename, int &length)
+	{
+		FILE *f = fopen(filename, "r");
+		void *buffer;
+
+		if (!f) {
+			fprintf(stderr, "Unable to open %s for reading\n", filename);
+			return NULL;
+		}
+
+		fseek(f, 0, SEEK_END);
+		length = ftell(f);
+		fseek(f, 0, SEEK_SET);
+
+		buffer = malloc(length+1);
+		length = fread(buffer, 1, length, f);
+		fclose(f);
+		((char*)buffer)[length] = '\0';
+
+		return buffer;
+	}
 }
 #endif // PhySketchUtils_h__
