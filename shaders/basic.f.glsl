072543ab71c77c4a9325ee4a16e47b2ea444bd19
abmantis <amfcalt@gmail.com>
2012-06-17 23:51:18 +0100

    Add base shaders

---
+++ shaders/basic.f.glsl
@@ -0,0 +1,7 @@
+#version 110
+
+void main()
+{
+    // Set the output color of our current pixel
+    gl_FragColor = gl_Color;
+}
---
+++ shaders/basic.v.glsl
@@ -0,0 +1,13 @@
+#version 110
+
+attribute vec2 position;
+uniform mat3 trans_mat;
+
+void main()
+{
+	vec3 transformedPos = trans_mat*vec3(position, 1);
+	gl_Position = gl_ModelViewProjectionMatrix * vec4(transformedPos, 1.0);
+//	gl_Position = vec4(position, 0.0, 1.0);
+	gl_FrontColor = gl_Color;  
+	gl_BackColor = gl_Color;  
+}
