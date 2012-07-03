8674ac4da6e0b3cea98ff6699b28f7ea81dafb43
abmantis <amfcalt@gmail.com>
2012-07-03 23:49:28 +0100

    Add isPointInTri()

--- include/PhySketchUtils.h
+++ include/PhySketchUtils.h
@@ -72,6 +72,22 @@ namespace PhySketch
 		return buffer;
 	}
 
+	static float sign(Vector2 p1, Vector2 p2, Vector2 p3)
+	{
+		return (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y);
+	}
+
+	static bool isPointInTri(Vector2 pt, Vector2 tri_v1, Vector2 tri_v2, Vector2 tri_v3)
+	{
+		bool b1, b2, b3;
+
+		b1 = sign(pt, tri_v1, tri_v2) < 0.0f;
+		b2 = sign(pt, tri_v2, tri_v3) < 0.0f;
+		b3 = sign(pt, tri_v3, tri_v1) < 0.0f;
+
+		return ((b1 == b2) && (b2 == b3));
+	}
+
 	/// <summary> Find the line-line intersection point. </summary>
 	/// <param name="start1"> The line 1 start point. </param>
 	/// <param name="end1"> The line 1 end point. </param>
