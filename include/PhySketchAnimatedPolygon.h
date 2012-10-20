3168ae15c655fa60261153db79c7c3bbb4724c75
abmantis <amfcalt@gmail.com>
2012-10-20 02:01:46 +0100

    Add blink support in AnimatedPolygon

--- include/PhySketchAnimatedPolygon.h
+++ include/PhySketchAnimatedPolygon.h
@@ -17,13 +17,29 @@ namespace PhySketch
 		virtual void translate( const Vector2& amount, float unitsPerSec );
 		virtual void setPosition( const Vector2& position, float unitsPerSec );
 
+		/// <summary> Blinks. </summary>
+		/// <param name="timeOn"> The time (in ms) to stay on. </param>
+		/// <param name="timeOff"> The time (in ms) to stay off. </param>
+		virtual void blink(ulong timeOn, ulong timeOff);
+
+		/// <summary> Stops blinking. </summary>
+		virtual void stopBlink(bool restoreAlpha);
+
 		virtual void update( ulong timeSinceLastFrame );		
 
 	protected:
+		// translation
 		Vector2 _desiredPosition;
 		Vector2 _translationDirection;
 		float _translationUnitsPerMs;
-		ulong _translationMsToComplete;	
+		ulong _translationMsToComplete;
+
+		// blink
+		ulong _blinkMsOn;
+		ulong _blinkMsOff;
+		ulong _blinkCurrStateMsToComplete;
+		float _blinkOriginalAlpha;
+
 		
 	};
 
--- src/PhySketchAnimatedPolygon.cpp
+++ src/PhySketchAnimatedPolygon.cpp
@@ -4,7 +4,8 @@ namespace PhySketch
 {
 AnimatedPolygon::AnimatedPolygon( VertexVariance vv /*= VV_Static*/, std::string name /*= ""*/ ) :
 	Polygon(vv, name),
-	_translationMsToComplete(0)
+	_translationMsToComplete(0),
+	_blinkCurrStateMsToComplete(0)
 {
 }
 
@@ -29,6 +30,7 @@ void AnimatedPolygon::setPosition( const Vector2& position, float unitsPerSec  )
 
 void AnimatedPolygon::update( ulong timeSinceLastFrame )
 {
+	// translate
 	if(_translationMsToComplete > 0)
 	{		
 		if(_translationMsToComplete > timeSinceLastFrame)
@@ -42,7 +44,58 @@ void AnimatedPolygon::update( ulong timeSinceLastFrame )
 			Polygon::setPosition(_desiredPosition);
 		}		
 	}
+
+	// blink
+	if(_blinkCurrStateMsToComplete > 0)
+	{		
+		if(_blinkCurrStateMsToComplete > timeSinceLastFrame)
+		{
+			_blinkCurrStateMsToComplete -= timeSinceLastFrame;
+		}
+		else
+		{
+			// toggle visibility
+			if(_alpha == 0)
+			{
+				_alpha = 1.0f;
+				_blinkCurrStateMsToComplete = _blinkMsOn;
+			}
+			else
+			{
+				_alpha = 0.0f;
+				_blinkCurrStateMsToComplete = _blinkMsOff;
+			}
+		}
+
+	}
 }
+
+void AnimatedPolygon::blink( ulong timeOn, ulong timeOff )
+{
+	_blinkMsOn = timeOn;
+	_blinkMsOff = timeOff;
+
+	_blinkOriginalAlpha = _alpha;
+
+	// toggle visibility
+	if(_alpha == 0)
+	{
+		_alpha = 1.0f;
+		_blinkCurrStateMsToComplete = _blinkMsOn;
+	}
+	else
+	{
+		_alpha = 0.0f;
+		_blinkCurrStateMsToComplete = _blinkMsOff;
+	}
+}
+
+void AnimatedPolygon::stopBlink( bool restoreAlpha )
+{
+	_blinkCurrStateMsToComplete = 0;
+	_alpha = _blinkOriginalAlpha;
+}
+
 
 
 
